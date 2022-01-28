8ad489cbb79af67186c0cc83fbea2a228172d83c
kstrempel <kai.strempel@takeaway.com>
2022-01-28 14:45:00 +0100

    More circles and refactoring

--- negative-space/src/main.rs
+++ negative-space/src/main.rs
@@ -6,35 +6,46 @@ fn main() {
 
 struct Model {
     _window: window::Id,
-    sectors: Vec<Vec<Point2>>
+    sectors: Vec<Vec<Point2>>,
 }
 
-fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
-    Model { 
-        _window,
-        sectors: Vec::new()
-    }
-}
-
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    let mut result = Vec::new();
+fn broken_circle(radius: f32, size: f32) -> Vec<Vec<Point2>> {
     let mut circle_inner = Vec::new();
     let mut circle_outer = Vec::new();
     let mut rad = 0.0;
     loop {
-        if rad > PI*2.0 {
+        if rad > PI * 2.0 {
             break;
         }
 
-        circle_inner.push(pt2(rad.sin() * 10.0, rad.cos() * 10.0));
-        circle_outer.push(pt2(rad.sin() * 15.0, rad.cos() * 15.0));
+        circle_inner.push(pt2(
+            rad.sin() * radius, 
+            rad.cos() * radius));
+        circle_outer.push(pt2(
+            rad.sin() * (radius + size),
+            rad.cos() * (radius + size),
+        ));
 
         rad += 0.01;
     }
-    
-    let circle_polygons = [circle_inner,circle_outer].concat();
-    result.push(circle_polygons);
+
+    vec![[circle_inner, circle_outer].concat()]
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    Model {
+        _window,
+        sectors: Vec::new(),
+    }
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let max_radius = (app.window_rect().h() / 2.0) as i32;
+    let mut result = Vec::new();
+    for i in (10..max_radius).step_by(40) {
+        result = [result, broken_circle(i as f32, 15.0)].concat();
+    }
     model.sectors = result;
 }
 
