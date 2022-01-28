a6a68f997f31b4d2c190569d9a53856e95191ebc
kstrempel <kai.strempel@takeaway.com>
2022-01-28 20:11:56 +0100

    first negative sets

--- negative-space/src/main.rs
+++ negative-space/src/main.rs
@@ -1,12 +1,19 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model).loop_mode(LoopMode::Wait).run();
 }
+
+struct Thing {
+    sectors: Vec<Vec<Point2>>,
+    rotate: f32,
+    color: Hsl
+}
+
 
 struct Model {
     _window: window::Id,
-    sectors: Vec<Vec<Point2>>,
+    things: Vec<Thing>
 }
 
 fn broken_circle(radius: f32, size: f32) -> Vec<Vec<Point2>> {
@@ -14,13 +21,11 @@ fn broken_circle(radius: f32, size: f32) -> Vec<Vec<Point2>> {
     let mut circle_outer = Vec::new();
     let mut rad = 0.0;
     loop {
-        if rad > PI * 2.0 {
+        if rad > PI * 1.5 {
             break;
         }
 
-        circle_inner.push(pt2(
-            rad.sin() * radius, 
-            rad.cos() * radius));
+        circle_inner.push(pt2(rad.sin() * radius, rad.cos() * radius));
         circle_outer.push(pt2(
             rad.sin() * (radius + size),
             rad.cos() * (radius + size),
@@ -29,32 +34,76 @@ fn broken_circle(radius: f32, size: f32) -> Vec<Vec<Point2>> {
         rad += 0.01;
     }
 
+    circle_outer.reverse();
     vec![[circle_inner, circle_outer].concat()]
 }
 
 fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
-    Model {
+    let _window = app
+        .new_window()
+        .view(view)
+        .mouse_pressed(mouse_pressed)
+        .build()
+        .unwrap();
+    let mut model = Model {
         _window,
-        sectors: Vec::new(),
-    }
+        things: Vec::new(),
+    };
+
+    update_model(app, &mut model);
+
+    model
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {
+fn mouse_pressed(app: &App, model: &mut Model, _mouse_button: MouseButton) {
+    update_model(app, model);
+}
+
+fn update_model(app: &App, model: &mut Model) {
     let max_radius = (app.window_rect().h() / 2.0) as i32;
     let mut result = Vec::new();
-    for i in (10..max_radius).step_by(40) {
-        result = [result, broken_circle(i as f32, 15.0)].concat();
+    for i in (50..max_radius).step_by(20) {
+        result.push(Thing { 
+            sectors: broken_circle(i as f32, 10.0),
+            rotate: random_f32() * TAU,
+            color: hsl(random_f32(), 0.7, 0.1)
+        });
     }
-    model.sectors = result;
+    model.things = result;
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
+    let wr = app.window_rect();
+    let left  = Rect::from_x_y_w_h(-1.0 * wr.w() / 2.0, 0.0, wr.w(), wr.h());
+    let right = Rect::from_x_y_w_h( wr.w() / 2.0, 0.0, wr.w(), wr.h());
 
-    for sector in model.sectors.iter() {
-        draw.polygon()
-            .points_colored(sector.iter().map(|&p| (p, STEELBLUE)));
+    draw.background().color(STEELBLUE);
+
+    draw.scissor(left)
+        .rect()
+        .x_y(left.x(), left.y())
+        .w_h(left.w(), left.h())
+        .color(gray(0.7));
+
+    draw.scissor(right)
+        .rect()
+        .x_y(right.x(), right.y())
+        .w_h(right.w(), right.h())
+        .color(BLACK);
+    
+    for t in model.things.iter() {
+        for s in t.sectors.iter() {
+            draw.scissor(left)
+                .polygon()
+                .points_colored(s.iter().map(|&p| (p, t.color)))
+                .rotate(t.rotate);
+
+            draw.scissor(right)
+                .polygon()
+                .points_colored(s.iter().map(|&p| (p, WHITE)))
+                .rotate(t.rotate);
+        }
     }
 
     draw.to_frame(app, &frame).unwrap();
